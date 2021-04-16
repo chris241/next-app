5bdcf7e481a7053e6bd17cbaf35f7d50eee9d4a5
eddy <eddy@dev.wazo.lu>
2021-04-16 18:02:43 +0300

    'zf'

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,39 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import  React from 'react';
+import { CookieBanner } from '@keepist/react-gdpr-cookie-banner';
+
 
 export default function Home() {
+
   return (
+
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      Bonjour à tous
+      <div className={styles.react}>
+      <CookieBanner  
+      message="Cookie banner message"
+      onAcceptPreferences = {() => { 
+        // load your preferences trackers here
+      }}
+      onAcceptStatistics = {() => {
+        // load your statistics trackers here
+      }}
+      onAcceptMarketing = {() => {
+        styles={
+          container
+        }
+      }}
+    />
+    </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
       </footer>
     </div>
   )
